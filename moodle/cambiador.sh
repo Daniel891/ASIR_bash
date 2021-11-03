6fd74c9bc5fb1d1bc2fb8cdc9e3aa32d0681e43f
dani <danielromero891@gmail.com>
2021-11-03 17:26:21 +0100

    intentando usar lo de los vectores

--- moodle/cambiador.sh
+++ moodle/cambiador.sh
@@ -2,12 +2,17 @@
 
 clear;
 
+declare A;
+
 while ! [ -z $1 ] && ! [ ${#1} -eq 3 ] && ! [ $# -eq 1  ]; do
 	echo "error";
 	exit;
 done
 
 if [[ $1 == +([[:digit:]]) ]]; then
+	for ((A=0;A<3;A++));do
+		echo ${1:A:3};
+	done
 	echo "va bien supongo";
 	exit;
 else echo "error son letras";
