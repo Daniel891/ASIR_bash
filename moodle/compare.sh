000682770decba1be145dbd0cb832acba01ead21
dani <danielromero891@gmail.com>
2021-11-16 12:52:14 +0100

    Casi terminado compare.sh

---
+++ moodle/compare.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+clear;
+
+if [ $# -eq 2 ] && [ -f $@ ];then
+	A=$(ls -l $1 | awk '{print $5}');
+	B=$(ls -l $2 | awk '{print $5}');
+	if [ $A -eq $B ];then
+		echo "Los dos archivos pesan lo mismo: $A";
+	elif [ $A -gt $B ];then
+		echo "El archivo $1 es el mayor de los dos. Tamaño(K): $A";
+	else echo "El archivo $2 es el mayor de los dos. Tamaño(K): $B";
+	fi
+	exit;
+else
+	echo "Uso: ./compare.sh archivo1 archivo2";
+fi
+
+exit;
