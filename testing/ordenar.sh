146d235906991eafa84fff17fd59a8ed1e865b43
dani <danielromero891@gmail.com>
2021-12-14 20:48:11 +0100

    Programa ordenar.sh que recibe cualquier cantidad de números y los ordena de manera ascedente

--- testing/ordenar.sh
+++ testing/ordenar.sh
@@ -3,6 +3,9 @@
 clear;
 
 declare C=0;
+declare P=0;
+declare I=1;
+declare T=0;
 
 for N;
 do
@@ -10,7 +13,23 @@ do
 	((C++));
 done
 
-while [ $I < ${#V} ];
+while (( $P <= ${#V[@]} ));
 do
-	if [  ];
-	then
+	while (( $I < ${#V[@]} ));
+	do
+		if [[ ${V[$P]} -lt ${V[$I]} ]];
+		then
+			((I++));
+		else
+			T=${V[$I]};
+			V[I]=${V[$P]};
+			V[P]=$T;
+			((I++));
+		fi
+	done
+
+	((P++));
+	I=$(($P+1));
+done
+
+echo ${V[@]};
